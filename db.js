e78d552c5d3ca6a280154108a9f0e2c1978dc358
Wepa Berdiyew <wberdiyew33@gmail.com>
2026-03-14 18:19:06 +0500

    sql update

--- db.js
+++ db.js
@@ -1,10 +1,5 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  password: 'wepa280106',
-  database: 'node_posgres',
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 
